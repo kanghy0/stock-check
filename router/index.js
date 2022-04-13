c253e8979f0d9f65e0292bbf01a40605445c8e3f
kanghy0 <hyk9094@gmail.com>
2022-04-13 22:08:46 +0900

    test

--- router/index.js
+++ router/index.js
@@ -11,47 +11,50 @@ router.get('/', (req, res) => {
 
 router.post('/data', (req, res) => {
   console.log('/posts/data call')
+  res.json({
+    test: 123,
+  })
   // const scm = req.scm;
   // if(scm == 'nelson') {
-    const goodsNo = req.goodsNo;
-    const optionNo = req.optionNo;
-    const size = req.size;
-    try {
-      axios.get('http://www.nelsonsports.co.kr/shop/goods/goods_view.php', {
-        params: {
-          goodsno: goodsNo,
-        }
-      })
-      .then(html => {
-        var $ = cheerio.load(html.data);
-        var content = $('div#content').children('script');
-        var script = '';
-        content.each(function(i, el) {
-          if(i == 5) {
-            script = $(this).html();
-          }
-        })
-        script = script.split('console.log(opt);');
-        script = script[0].split("opt['" + optionNo + "']");
-        var isSoldout = script[1].search("'" + size + "','soldout'");
-        if(isSoldout == -1) {
-          isSoldout = 1;
-        } else if(1) {
-          isSoldout = 0;
-        } else {
-          isSoldout = -1;
-        }
-        res.json({
-          isSoldout: isSoldout,
-        });
-      })
-      .catch(err => {
-        console.log(err);
-      })
-    }
-    catch (err) {
-      console.log(err)
-    }     
+    // const goodsNo = req.goodsNo;
+    // const optionNo = req.optionNo;
+    // const size = req.size;
+    // try {
+    //   axios.get('http://www.nelsonsports.co.kr/shop/goods/goods_view.php', {
+    //     params: {
+    //       goodsno: goodsNo,
+    //     }
+    //   })
+    //   .then(html => {
+    //     var $ = cheerio.load(html.data);
+    //     var content = $('div#content').children('script');
+    //     var script = '';
+    //     content.each(function(i, el) {
+    //       if(i == 5) {
+    //         script = $(this).html();
+    //       }
+    //     })
+    //     script = script.split('console.log(opt);');
+    //     script = script[0].split("opt['" + optionNo + "']");
+    //     var isSoldout = script[1].search("'" + size + "','soldout'");
+    //     if(isSoldout == -1) {
+    //       isSoldout = 1;
+    //     } else if(1) {
+    //       isSoldout = 0;
+    //     } else {
+    //       isSoldout = -1;
+    //     }
+    //     res.json({
+    //       isSoldout: isSoldout,
+    //     });
+    //   })
+    //   .catch(err => {
+    //     console.log(err);
+    //   })
+    // }
+    // catch (err) {
+    //   console.log(err)
+    // }     
   // }
   // return false;
 });
