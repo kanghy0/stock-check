13dfb8441992e431c9ee356c4ab76e134e94fac6
kanghy0 <hyk9094@gmail.com>
2022-04-26 19:11:49 +0900

    test

--- router/index.js
+++ router/index.js
@@ -29,7 +29,7 @@ router.post('/nelson', (req, res) => {
     const optionNo = req.body.optionNo;
     const size = req.body.size;
     if(!goodsNo || !optionNo || !size) {
-      res.json(optionNo);
+      res.json(goodsNo);
     } else {
       try {
         axios.get('http://www.nelsonsports.co.kr/shop/goods/goods_view.php', {
